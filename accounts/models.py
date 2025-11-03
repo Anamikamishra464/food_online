e743c05f29a213db3936df700b1fcefc4614f607
Anamika Mishra <mishraanamika11a@gmail.com>
2025-11-03 13:13:16 +0530

    vendor registration

--- accounts/models.py
+++ accounts/models.py
@@ -39,10 +39,10 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
-    RESTAURANT=1
+    VENDOR=1
     CUSTOMER=2
     ROLE_CHOICE=(
-        (RESTAURANT,'restaurant'),
+        (VENDOR,'Vendor'),
         (CUSTOMER, 'customer'),
     )
     first_name=models.CharField(max_length=50)
